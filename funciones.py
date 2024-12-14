f94ee5609e15b6d3856792a6b27098b378bad781
sebastianelgue <sebaelgue00@gmail.com>
2024-12-14 20:20:15 -0300

    historial de juego

--- funciones.py
+++ funciones.py
@@ -1,8 +1,10 @@
 from colorama import init
 from mensajes import (
     BIENVENIDA, INTRODUCCION, REGLA_CORRECTA, NUEVA_REGLA,
-    VICTORIA, ERROR_CONTRASENA, REINTENTO, RONDA, mostrar_error_regla
+    VICTORIA, ERROR_CONTRASENA, REINTENTO, RONDA, mostrar_error_regla,
+    FINAL_JUEGO, HISTORIAL, ESTADISTICA
 )
+import time
 
 init(autoreset=True)
 
@@ -15,34 +17,63 @@ def verificar_reglas(contraseña, reglas_aplicadas):
     return errores
 
 
+def mostrar_historial(tiempo_total, errores_totales, intentos_totales, ronda):
+    print(HISTORIAL)
+    print(ESTADISTICA.format("Tiempo total jugado", str(round(tiempo_total, 2))
+                             + " segundos"))
+    print(ESTADISTICA.format("Total de errores cometidos",
+                             str(errores_totales)))
+    print(ESTADISTICA.format("Total de contraseñas intentadas",
+                             str(intentos_totales)))
+    print(ESTADISTICA.format("Rondas completadas", str(ronda)))
+    print(FINAL_JUEGO)
+
+
 def jugar(reglas):
     reglas_aplicadas = [reglas[0]]  # Arranca con la primera regla
     ronda = 1
+    intentos_totales = 0
+    errores_totales = 0
     juego_terminado = False
 
     print(BIENVENIDA)
     print(INTRODUCCION)
+    print("Para finalizar el juego, introduce FIN.")
     print(f"\nRegla: {reglas_aplicadas[-1][0]}")
+
+    # Registrar el tiempo de inicio
+    tiempo_inicio = time.time()
 
     while not juego_terminado:
         print(RONDA.format(ronda))
         contraseña = input("Introduce una contraseña: ")
 
-        errores = verificar_reglas(contraseña, reglas_aplicadas)
-
-        if not errores:
-            print(REGLA_CORRECTA)
-
-            # Agrega una nueva regla
-            if len(reglas_aplicadas) < len(reglas):
-                reglas_aplicadas.append(reglas[len(reglas_aplicadas)])
-                print(NUEVA_REGLA, reglas_aplicadas[-1][0], "\n")
-                ronda += 1
-            else:
-                print(VICTORIA)
-                juego_terminado = True
+        if contraseña.upper() == "FIN":
+            juego_terminado = True
         else:
-            print(ERROR_CONTRASENA)
-            for error in errores:
-                print(mostrar_error_regla(error))
-            print(REINTENTO)
+            intentos_totales += 1
+            errores = verificar_reglas(contraseña, reglas_aplicadas)
+
+            if not errores:
+                print(REGLA_CORRECTA)
+
+                if len(reglas_aplicadas) < len(reglas):
+                    reglas_aplicadas.append(reglas[len(reglas_aplicadas)])
+                    print(NUEVA_REGLA, reglas_aplicadas[-1][0], "\n")
+                    ronda += 1
+                else:
+                    print(VICTORIA)
+                    juego_terminado = True
+            else:
+                print(ERROR_CONTRASENA)
+                errores_totales += len(errores)
+                for error in errores:
+                    print(mostrar_error_regla(error))
+                print(REINTENTO)
+
+    # Registrar el tiempo de finalización
+    tiempo_fin = time.time()
+    tiempo_total = tiempo_fin - tiempo_inicio
+
+    # Mostrar estadísticas del juego
+    mostrar_historial(tiempo_total, errores_totales, intentos_totales, ronda)
--- mensajes.py
+++ mensajes.py
@@ -1,37 +1,48 @@
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 # Mensajes de inicio
-BIENVENIDA = Fore.CYAN + Back.BLACK + Style.BRIGHT + "\nThe Password Game:"
+BIENVENIDA = Fore.CYAN + Style.BRIGHT + "\nThe Password Game:"
 INTRODUCCION = (
-    Fore.CYAN + Back.BLACK + Style.BRIGHT +
+    Fore.CYAN + Style.BRIGHT +
     "\nIntroduce una contraseña que cumpla con las siguientes reglas:"
+)
+FINAL_JUEGO = (
+    Fore.LIGHTYELLOW_EX + Style.BRIGHT + "\n{}\n"
 )
 
 # Mensajes de reglas
 REGLA_CORRECTA = (
-    Fore.LIGHTGREEN_EX + Back.BLACK + Style.BRIGHT +
+    Fore.LIGHTGREEN_EX + Style.BRIGHT +
     "\n¡Contraseña correcta!"
 )
 NUEVA_REGLA = (
-    Fore.LIGHTBLUE_EX + Back.BLACK + Style.BRIGHT +
+    Fore.LIGHTBLUE_EX + Style.BRIGHT +
     "Nueva regla añadida:"
 )
 
 # Mensajes de ganar juego
 VICTORIA = (
-    Fore.LIGHTGREEN_EX + Back.BLACK + Style.BRIGHT +
+    Fore.LIGHTGREEN_EX + Style.BRIGHT +
     "¡Has ganado! Todas las reglas han sido aplicadas."
 )
 
 # Mensajes de error
 ERROR_CONTRASENA = (
-    Fore.LIGHTRED_EX + Back.BLACK + Style.BRIGHT +
+    Fore.LIGHTRED_EX + Style.BRIGHT +
     "\nLa contraseña no cumple con las siguientes reglas:"
 )
 REINTENTO = "Por favor, inténtalo de nuevo.\n"
 
 # Mensaje para la ronda
 RONDA = Fore.WHITE + Style.BRIGHT + "\nRonda {}:"
+
+# Mensajes de estadísticas
+HISTORIAL = (
+    Fore.LIGHTCYAN_EX + Style.BRIGHT + "\nEstadísticas del juego:\n"
+)
+ESTADISTICA = (
+    Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "- {}: {}"
+)
 
 
 # Función para mostrar el error
